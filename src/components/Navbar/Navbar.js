aa39ccfda06592cac5f0b9c3747dea748a537cdf
Michael Nguyen <n93michael@gmail.com>
2022-10-06 12:58:58 -0400

    navbar setup

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import "./Navbar.scss";
+import { Fragment } from "react";
+
+function Navbar() {
+  return (
+    <Fragment>
+      <h1>Hello from nav</h1>
+    </Fragment>
+  );
+}
+
+export default Navbar;
