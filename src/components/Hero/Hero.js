fe715c849e2b3ab4acfe4fc6b6d0a9163414fafd
Michael Nguyen <n93michael@gmail.com>
2022-10-07 22:24:28 -0400

    hero animations done

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,44 +1,106 @@
 import './Hero.scss';
 import { useEffect } from 'react';
 import gsap, { Power4 } from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
 
 function Hero() {
   const headerAnimation = () => {
     //Initialize timeline
     const tl = gsap.timeline();
 
-    tl.from('.landing__period', {
-      y: -250,
-      opacity: 0,
-      duration: 0.5,
-      stagger: 0.2,
-      ease: Power4.easeOut,
-    })
+    tl
+      //Starting animation after loading
+      .from('.landing__period', {
+        opacity: 0,
+        y: -250,
+        duration: 0.5,
+        stagger: 0.2,
+        ease: Power4.easeOut,
+      })
       .to('.landing__periods', {
         yPercent: 100,
         duration: 1.5,
         ease: Power4.easeInOut,
       })
       .from(
-        '.landing__author-text',
+        '.landing__first-name',
         {
-          y: 250,
-          duration: 0.8,
+          y: -250,
+          duration: 1,
+          opacity: 0,
+          stagger: 0.1,
+          ease: Power4.easeOut,
         },
-        '-=0.8'
+        '-=1'
+      )
+      .from(
+        '.landing__last-name',
+        {
+          y: -250,
+          duration: 1,
+          opacity: 0,
+          stagger: 0.1,
+          ease: Power4.easeOut,
+        },
+        '-=0.5'
       );
+  };
+
+  const authorNameAnimation = () => {
+    gsap.to('.landing__first-name', {
+      y: -250,
+      immediateRender: false,
+      stagger: -0.1,
+      scrollTrigger: {
+        trigger: '.landing__author',
+        markers: true,
+        start: 'top top',
+        end: 'bottom top',
+        scrub: 4,
+      },
+    });
+
+    gsap.to('.landing__last-name', {
+      y: -250,
+      immediateRender: false,
+      stagger: -0.1,
+      scrollTrigger: {
+        trigger: '.landing__author',
+        markers: true,
+        start: 'top top',
+        end: 'bottom top',
+        scrub: 4,
+      },
+    });
   };
 
   useEffect(() => {
     headerAnimation();
+    authorNameAnimation();
   }, []);
 
   return (
     <section className="landing">
-      <p className="landing__author">
-        <span className="landing__author-text">Michael Nguyen</span>
-      </p>
-
+      <div className="landing__author">
+        <p className="landing__name">
+          <span className="landing__first-name">M</span>
+          <span className="landing__first-name">I</span>
+          <span className="landing__first-name">C</span>
+          <span className="landing__first-name">H</span>
+          <span className="landing__first-name">A</span>
+          <span className="landing__first-name">E</span>
+          <span className="landing__first-name">L</span>
+        </p>
+        <p className="landing__name">
+          <span className="landing__last-name">N</span>
+          <span className="landing__last-name">G</span>
+          <span className="landing__last-name">U</span>
+          <span className="landing__last-name">Y</span>
+          <span className="landing__last-name">E</span>
+          <span className="landing__last-name">N</span>
+        </p>
+      </div>
       <p className="landing__periods">
         <span className="landing__period"></span>
         <span className="landing__period"></span>
