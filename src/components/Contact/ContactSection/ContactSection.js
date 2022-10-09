9bed9bd9283b68490aff7be69a897b4f2a9f5c8a
Michael Nguyen <n93michael@gmail.com>
2022-10-08 23:45:32 -0400

    contact section jsx and styling done

---
+++ src/components/Contact/ContactSection/ContactSection.js
@@ -0,0 +1,51 @@
+import './ContactSection.scss';
+import emailIcon from '../../../assets/icons/email-icon.svg';
+
+function ContactSection() {
+  return (
+    <section className="contact">
+      {/* <h2 className="contact__header">CONTACT ME</h2> */}
+
+      <div className="contact__container">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/michael-nguyenn"
+          className="contact__link"
+        >
+          <img
+            src="https://raw.githubusercontent.com/devicons/devicon/v2.15.1/icons/github/github-original.svg"
+            alt="GitHub Icon"
+            className="contact__img"
+          />
+        </a>
+
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/michael--nguyen/"
+          className="contact__link"
+        >
+          <img
+            src="https://raw.githubusercontent.com/devicons/devicon/v2.15.1/icons/linkedin/linkedin-plain.svg"
+            alt="GitHub Icon"
+            className="contact__img"
+          />
+        </a>
+
+        <a
+          target="_blank"
+          aria-label="Michael Nguyen - Getting in touch"
+          rel="noreferrer"
+          draggable="false"
+          href="mailto:n93michael@gmail.com?subject=Getting%20In%20Touch"
+          className="contact__link"
+        >
+          <img src={emailIcon} alt="Icon of Mail" className="contact__img" />
+        </a>
+      </div>
+    </section>
+  );
+}
+
+export default ContactSection;
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,12 +2,14 @@ import ContactForm from '../Contact/ContactForm/ContactForm';
 import ContactBanner from '../Contact/ContactBanner/ContactBanner';
 
 import './Footer.scss';
+import ContactSection from '../Contact/ContactSection/ContactSection';
 
 function Footer() {
   return (
     <footer>
       {/* <ContactForm /> */}
       <ContactBanner />
+      <ContactSection />
     </footer>
   );
 }
