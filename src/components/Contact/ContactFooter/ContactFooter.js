fa59183d39968059864bdbe7f9fa198cd589e87e
Michael Nguyen <n93michael@gmail.com>
2022-10-14 21:35:41 -0400

    footer credit styling

--- src/components/Contact/ContactFooter/ContactFooter.js
+++ src/components/Contact/ContactFooter/ContactFooter.js
@@ -1,4 +1,5 @@
 import './ContactFooter.scss';
+import hand from '../../../assets/icons/hands.png';
 
 function ContactFooter() {
   return (
@@ -9,6 +10,30 @@ function ContactFooter() {
           <div className="landing__star2"></div>
           <div className="landing__star3"></div>
         </div>
+
+        <div className="contact-credit">
+          <img
+            className="contact-credit__hand contact-credit__hand--left"
+            src={hand}
+            alt="Left Hand"
+            loading="lazy"
+          />
+
+          <img
+            className="contact-credit__hand contact-credit__hand--right"
+            src={hand}
+            alt="Left Right"
+            loading="lazy"
+          />
+        </div>
+        <a
+          href="https://github.com/michael-nguyenn/portfolio"
+          target="_blank"
+          rel="noreferrer"
+          className="contact-credit__text"
+        >
+          Michael Nguyen
+        </a>
       </div>
     </div>
   );
