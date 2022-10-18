ebdf097d9f82dc21f3eb8fb05cdf1bf9579a8096
Michael Nguyen <n93michael@gmail.com>
2022-10-18 19:00:37 -0400

    animations for footer credits

--- src/components/Contact/ContactFooter/ContactFooter.js
+++ src/components/Contact/ContactFooter/ContactFooter.js
@@ -1,7 +1,48 @@
 import './ContactFooter.scss';
 import hand from '../../../assets/icons/hands.png';
+import gsap, { Power4 } from 'gsap';
+import { useEffect, useRef } from 'react';
 
 function ContactFooter() {
+  //Create ref
+  const leftHandRef = useRef(null);
+  const rightHandRef = useRef(null);
+
+  //Anime on mouse over
+  const mouseOverHandler = () => {
+    leftHandRef.current = gsap.to(leftHandRef.current, {
+      xPercent: 15,
+      translateY: -1,
+      rotate: -25,
+      duration: 0.5,
+      paused: true,
+      ease: Power4.easeInOut,
+    });
+
+    rightHandRef.current = gsap.to(rightHandRef.current, {
+      xPercent: -15,
+      translateY: 1,
+      duration: 0.5,
+      paused: true,
+      rotate: 150,
+      ease: Power4.easeInOut,
+    });
+  };
+
+  const handleMouseOverText = () => {
+    leftHandRef.current.play();
+    rightHandRef.current.play();
+  };
+
+  const handleMouseLeaveText = () => {
+    leftHandRef.current.reverse();
+    rightHandRef.current.reverse();
+  };
+
+  useEffect(() => {
+    mouseOverHandler();
+  });
+
   return (
     <div className="contact-footer">
       <div className="contact-footer">
@@ -11,12 +52,24 @@ function ContactFooter() {
           <div className="landing__star3"></div>
         </div>
 
+        <a
+          href="https://github.com/michael-nguyenn/portfolio"
+          target="_blank"
+          rel="noreferrer"
+          className="contact-credit__text"
+          onMouseEnter={() => handleMouseOverText()}
+          onMouseLeave={() => handleMouseLeaveText()}
+        >
+          Michael Nguyen
+        </a>
+
         <div className="contact-credit">
           <img
             className="contact-credit__hand contact-credit__hand--left"
             src={hand}
             alt="Left Hand"
             loading="lazy"
+            ref={leftHandRef}
           />
 
           <img
@@ -24,16 +77,9 @@ function ContactFooter() {
             src={hand}
             alt="Left Right"
             loading="lazy"
+            ref={rightHandRef}
           />
         </div>
-        <a
-          href="https://github.com/michael-nguyenn/portfolio"
-          target="_blank"
-          rel="noreferrer"
-          className="contact-credit__text"
-        >
-          Michael Nguyen
-        </a>
       </div>
     </div>
   );
